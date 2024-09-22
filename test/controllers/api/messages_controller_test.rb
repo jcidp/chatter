672faa856f78f35c15a7a026fac5a9f1e6aa7fda
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-09-21 20:07:06 -0600

    Add messages controller tests

--- test/controllers/api/messages_controller_test.rb
+++ test/controllers/api/messages_controller_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 
 class Api::MessagesControllerTest < ActionDispatch::IntegrationTest
-  # test "should get create" do
-  #   get api_messages_create_url
-  #   assert_response :success
-  # end
+  setup do
+    @user, @token = sign_in_as(users(:lazaro_nixon))
+  end
+
+  def default_headers
+    { "Authorization" => "Bearer #{@token}" }
+  end
+
+  test "should create new message" do
+    chat = chats(:one)
+    test_text = "Test text"
+    assert_difference("Message.count") do
+      post api_messages_url, params: { chat_id: chat.id, text: test_text }, headers: default_headers
+    end
+    assert_equal test_text, chat.messages.last.text
+  end
 end

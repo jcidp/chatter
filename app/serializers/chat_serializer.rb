2e1745d44fa6f86fc1841817977be350896876b9
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-10-12 20:08:32 -0600

    Add other_user method to DRY chat serializer

--- app/serializers/chat_serializer.rb
+++ app/serializers/chat_serializer.rb
@@ -3,31 +3,32 @@ class ChatSerializer < ActiveModel::Serializer
 
   attribute :last_message, if: :include_last_message?
   has_many :messages, if: :include_messages?
-
+  
   def name
-    other_user = object.users.find { |user| user != instance_options[:current_user] }
     other_user&.username
   end
-
+  
   def last_message
     last_message = object.messages.last
     last_message&.as_json(only: [:text, :created_at])
   end
-
+  
   def image
-    other_user = object.users.find { |user| user != instance_options[:current_user] }
     UserSerializer.new(other_user).avatar if other_user
   end
-
+  
   def profile_id
-    other_user = object.users.find { |user| user != instance_options[:current_user] }
     other_user&.id
   end
-
+  
   def messages
     object.messages.sorted
   end
-
+  
+  def other_user
+    @other_user ? @other_user : @other_user = object.users.find { |user| user != instance_options[:current_user] }
+  end
+  
   def include_messages?
     instance_options[:include_messages]
   end

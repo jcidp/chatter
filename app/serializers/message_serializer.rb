770211f96faabb11bc5142cb08ee469ce33205b7
jcidp <86532479+jcidp@users.noreply.github.com>
2024-11-17 00:55:37 -0600

    Add id & author to message serializer

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,8 +1,12 @@
 class MessageSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :text, :user_id, :chat_id, :created_at, :image
+  attributes :id, :text, :user_id, :chat_id, :created_at, :image, :author
 
   def image
     "#{Rails.configuration.x.api_base_url}#{rails_blob_path(object.image, only_path: true)}" if object.image.attached?
+  end
+
+  def author
+    object.author.username
   end
 end

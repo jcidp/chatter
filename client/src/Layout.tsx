ba313bae8bba8a4d5acd604fb44e6cb608baa4e0
jcidp <86532479+jcidp@users.noreply.github.com>
2024-11-17 13:18:09 -0600

    Move ThemeProvider above the Router

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,13 +1,10 @@
 import { Outlet } from "react-router-dom";
 import AuthProvider from "./helpers/AuthProvider";
-import { ThemeProvider } from "./helpers/ThemeProvider";
 
 const Layout = () => {
   return (
     <AuthProvider>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <Outlet />
-      </ThemeProvider>
+      <Outlet />
     </AuthProvider>
   );
 };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./Router.tsx";
+import { ThemeProvider } from "./helpers/ThemeProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router />
+    </ThemeProvider>
   </StrictMode>,
 );

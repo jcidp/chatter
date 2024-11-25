29bb433b5a7d28ca14548f5b1a99a00d2d08081f
jcidp <86532479+jcidp@users.noreply.github.com>
2024-11-24 19:35:11 -0600

    Create db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,47 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+users = [
+  {
+    username: "chatter_bot",
+    bio: "A helpful bot that welcomes you to Chatter"
+  },
+  {
+    username: "sarah-miller12",
+    bio: "Lets chat!"
+  },
+  {
+    username: "edith.rhodes93"
+  },
+  {
+    username: "raymsym",
+    bio: "Available"
+  },
+  {
+    username: "joe_cool9",
+    bio: ";)"
+  },
+  {
+    username: "scott_kelly_the_king",
+    bio: "Baltimore born and raised! Probably cracking open a cold one with the boys"
+  }
+]
+
+users.each do |user|
+  User.find_or_create_by!(
+    username: user[:username],
+    email: "#{user[:username]}@email.com",
+    bio: user[:bio]
+  ) do |new_user|
+    new_user.password = "Pas$word1234"
+  end
+end
+
+chat = Chat.first || Chat.create!
+Group.find_or_create_by!(
+  name: "New users group",
+  description: "Say hi to other new users!",
+  chat_id: chat.id
+)
+chat.chat_users.create!(user_id: 1, is_admin: true)
+chat.messages.create!(text: "Welcome everyone! Feel free to send a message when you join", user_id: 1)
